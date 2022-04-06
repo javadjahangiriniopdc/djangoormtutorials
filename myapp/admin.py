397cca572ed4927198b990bbfba42a35122c6820
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-04-06 01:06:42 -0700

    add teacher Modol

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from myapp.models import Student
+from myapp.models import Student, Teacher
 
 admin.site.register(Student)
+admin.site.register(Teacher)
--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=200)
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.teacher_name}'
+
 
 class Student(models.Model):
     username = models.CharField(max_length=20)
@@ -11,6 +14,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=30)
     mobile = models.CharField(max_length=10)
     email = models.EmailField()
+    teacher = models.ForeignKey(Teacher, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
